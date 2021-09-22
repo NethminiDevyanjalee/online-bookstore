96a658f8da2148d345dd22d4db82f09755a444c6
29Niro <29niro@gmail.com>
2021-09-22 11:11:40 +0530

    User books view module added

--- index.php
+++ index.php
@@ -1,51 +1,70 @@
-<?php require_once "header.php" ?>
+<?php
+require "admin/core/db.php";
+/* pagination */
+$sql = "SELECT COUNT(id) AS count FROM book WHERE is_delete = '0' ";
+$count = $conn->query($sql)->fetch_array()['count'];
+$limit = 20;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$num_pages = ceil($count / $limit);
+$start = ($page - 1) * $limit;
 
+$sql = "SELECT * FROM `online-bookstore`.book
+        WHERE is_delete = '0'  
+        LIMIT {$start},{$limit}";
+$books = $conn->query($sql);
+?>
+
+<?php require_once "header.php" ?>
 <main>
-    <div class="container my-5">
+    <div class="container">
+
         <div class="row row-cols-1 row-cols-md-4 g-4">
-            <?php for ($i = 0;
-                       $i < 20;
-                       $i++): ?>
+            <?php while ($book = $books->fetch_array()): ?>
                 <div class="col">
                     <div class="card">
-
                         <div class="card-body">
-                            <!--                    <div class="icon-circle bg-primary"><i class="fas fa-heart"></i></div>-->
-                            <!--                    <div class="ribbon ribbon-top-left"><span>OUT OF STOCK</span></div>-->
                             <div class="item-pic item-img-hov">
-
-                                <img src="https://online-bookstore.azurewebsites.net/admin/uploads/book-img-c4ca4238a0b923820dcc509a6f75849b.jpg"
-                                     alt="IMG-PRODUCT">
+                                <img src="<?= $book['img_url']?>" alt="<?= $book['slug']?>">
                                 <a href="#" class="item-btn flex-c-m item-btn-font item-btn-hov item-trans">
                                     Quick View
                                 </a>
                             </div>
-                            <h5 class="card-title theme-text-title mt-1">Book title</h5>
-                            <small class="theme-text text-muted">This is a longer card with supporting text below as a
-                                natural
-                                lead-in
-                                to additional content. This content is a little bit longer.</small>
-                            <div class="row justify-content-between">
-                                <div class="col">
-                                    <div class="rating-bar" data-rate="4.2" data-max="5"></div>
-                                </div>
-                                <div class="col">
-                                    <h5 class="theme-text-title text-end theme-primary-color">LKR 300</h5>
-                                </div>
-                            </div>
-                            <div class="d-flex justify-content-center">
-                                <button class="theme-btn theme-btn-dark-animated theme-font-bold">
-                                    <i class="fa fa-cart-plus" aria-hidden="true"></i>&nbsp;Add to Cart
-                                </button>
-                            </div>
+                            <h5 class="card-title"><?= $book['name'] ?></h5>
+                            <span style="display: -webkit-box;
+                                -webkit-line-clamp: 4;
+                                -webkit-box-orient: vertical;
+                                overflow: hidden;
+                                text-overflow: ellipsis;"><?= $book['description']?>
+                            </span>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
-            <?php endfor; ?>
+            <?php endwhile; ?>             
         </div>
+        <div class="card-footer">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination float-end">
+                    <li class="page-item <?php if ($page == 1) echo 'disabled'; ?>">
+                        <a class="page-link" href="<?= change_url_params('page', $page - 1) ?>">
+                            Previous
+                        </a>
+                    </li>
+                    <?php for ($i = 1; $i <= $num_pages; $i++): ?>
+                    <li class="page-item <?php if ($page == $i) echo 'active'; ?>">
+                        <a class="page-link" href="<?= change_url_params('page', $i) ?>">
+                            <?= $i ?>
+                        </a>
+                    </li>
+                    <?php endfor; ?>
+                    <li class="page-item <?php if ($page == $num_pages) echo 'disabled'; ?>">
+                        <a class="page-link"href="<?= change_url_params('page', $page + 1) ?>">
+                            Next
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>           
     </div>
-
 </main>
-
 <?php require_once "footer.php" ?>
-
